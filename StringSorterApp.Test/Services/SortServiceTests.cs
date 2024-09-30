09bbd9ea96eb99f1ccb209a6f8aa71a61ccefabc
Josiah Matela <90241728+jmatela26@users.noreply.github.com>
2024-09-30 15:36:11 +0800

    Add files via upload

---
+++ StringSorterApp.Test/Services/SortServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using StringSorterApp.Dto;
+using StringSorterApp.Enums;
+using StringSorterApp.Service.SortService;
+
+namespace StringSorterApp.Test.Services
+{
+    public class SortServiceTests
+    {
+        private readonly SortService _sortService;
+
+        public SortServiceTests()
+        {
+            _sortService = new SortService();
+        }
+
+        [Fact]
+        public void BubbleSort_ShouldReturnSortedString()
+        {
+            // Arrange
+            var requestDto = new SortRequestDto
+            {
+                InputString = "dcba",
+                SortStrategy = SortStrategyType.BubbleSort
+            };
+
+            // Act
+            var result = _sortService.SortString(requestDto);
+
+            // Assert
+            Assert.Equal("abcd", result.SortedString);
+        }
+
+        [Fact]
+        public void QuickSort_ShouldReturnSortedString()
+        {
+            // Arrange
+            var requestDto = new SortRequestDto
+            {
+                InputString = "gfedcba",
+                SortStrategy = SortStrategyType.QuickSort
+            };
+
+            // Act
+            var result = _sortService.SortString(requestDto);
+
+            // Assert
+            Assert.Equal("abcdefg", result.SortedString);
+        }
+
+        [Fact]
+        public void Sort_ShouldReturnEmptyString_WhenInputIsEmpty()
+        {
+            // Arrange
+            var requestDto = new SortRequestDto
+            {
+                InputString = "",
+                SortStrategy = SortStrategyType.BubbleSort
+            };
+
+            // Act
+            var result = _sortService.SortString(requestDto);
+
+            // Assert
+            Assert.Equal("", result.SortedString);
+        }
+
+        [Fact]
+        public void Sort_ShouldThrowException_WhenStrategyIsInvalid()
+        {
+            // Arrange
+            var requestDto = new SortRequestDto
+            {
+                InputString = "abc",
+                SortStrategy = (SortStrategyType)999 // Invalid enum value
+            };
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => _sortService.SortString(requestDto));
+        }
+    }
+}
+
